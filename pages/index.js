b80368daec919df3d43a13ff0d0cdbd760d7527d
Bart Nagel <bart@tremby.net>
2020-12-04 18:50:51 -0800

    Improvements

--- pages/index.js
+++ pages/index.js
@@ -27,24 +27,7 @@ const MobileOnly = styled.p`
 	color: green;
 `
 
-export default function Home() {
-	// If you set this to false and refresh with a narrow viewport, you'll
-	// get a broken view, even if you then resize wider/narrower.
-	//
-	// If you set this to true and refresh with a wide viewport, you'll get
-	// a different broken view, even if you then resize narrower/wider.
-	//
-	// You only see the correct view if mobile is initialized to false and
-	// you refresh with a narrow viewport (it's then still correct with
-	// resizes) or you initialize it to true and you refresh with a wide
-	// viewport (it's then still correct with resizes).
-	let mobile = false;
-
-	try {
-		mobile = useBreakpoint(down("sm"));
-	} catch {
-		// Do nothing; we're probably doing SSR
-	}
+function Content({ mobile }) {
 	return (
 		<Container>
 			<All1>all 1 (red)</All1>
@@ -59,5 +42,63 @@ export default function Home() {
 				<MobileOnly>mobile only (green)</MobileOnly>
 			)}
 		</Container>
+	);
+}
+
+function InitMobile() {
+	let mobile = true;
+	try {
+		mobile = useBreakpoint(down("sm"));
+	} catch {
+		// Do nothing; we're probably doing SSR
+	}
+	return <Content mobile={mobile} />;
+}
+
+function InitDesktop() {
+	let mobile = false;
+	try {
+		mobile = useBreakpoint(down("sm"));
+	} catch {
+		// Do nothing; we're probably doing SSR
+	}
+	return <Content mobile={mobile} />;
+}
+
+export default function Home() {
+	// If the viewport width hint is initialized to false (i.e. SSR as if
+	// desktop width), you'll get a broken view, even if you then resize
+	// wider/narrower.
+	//
+	// If the viewport width hint is initialized to true (i.e. SSR as if
+	// mobile width), you'll get a different broken view, even if you then
+	// resize narrower/wider.
+	//
+	// You only see the correct view if the page loads when the width is the
+	// same as how the hint was initialized. It's then still correct with
+	// resizes.
+
+	const [initialMobile, setInitialMobile] = React.useState(null);
+	let currentMobile = null;
+	try {
+		currentMobile = useBreakpoint(down("sm"));
+	} catch {
+		// Do nothing; we're probably doing SSR
+	}
+	if (initialMobile == null && currentMobile != null) {
+		setInitialMobile(currentMobile);
+	}
+
+	return (
+		<>
+			<h2>Width on initial render</h2>
+			<p>{initialMobile === true ? "mobile" : initialMobile === false ? "desktop" : "unknown"}</p>
+
+			<h2>Width hint initialized to mobile</h2>
+			<InitMobile />
+
+			<h2>Width hint initialized to desktop</h2>
+			<InitDesktop />
+		</>
 	);
 }
